95ed644d57e384ed8f2d769a66993bc7af647ef5
Muhammad Aktharuzzaman <akth8r@hotmail.com>
2018-08-15 13:02:12 +0100

    Added the current month and year to the UI. Also added some colour variation to distinguish between income and expense.

--- app.js
+++ app.js
@@ -173,7 +173,8 @@ var UIController = (function() {
         expensesLabel: '.budget__expenses--value',
         percentageLabel: '.budget__expenses--percentage',
         container: '.container',
-        expensesPercLabel: '.item__percentage'
+        expensesPercLabel: '.item__percentage',
+        dateLabel: '.budget__title--month'
     }
 
     var formatNumber = function(num, type) {
@@ -201,6 +202,14 @@ var UIController = (function() {
         dec = numSplit[1];
 
         return (type === 'exp' ? '-' : '+') + ' ' + int + '.' + dec;
+
+    };
+
+    var nodeListForEach = function (list, callback) {
+
+        for (var i = 0; i < list.length; i++) {
+            callback(list[i], i);
+        }
 
     };
 
@@ -289,14 +298,6 @@ var UIController = (function() {
 
             var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
 
-            var nodeListForEach = function(list, callback) {
-
-                for(var i = 0; i < list.length; i++) {
-                    callback(list[i], i);
-                }
-
-            };
-
             nodeListForEach(fields, function(current, index) {
 
                 if(percentages[index] > 0){
@@ -306,6 +307,39 @@ var UIController = (function() {
                 }
 
             });
+
+        },
+
+        displayDate: function() {
+            var now, year, month, months;
+
+            now = new Date();
+            // var christmas = new Date(2016, 11, 25);
+
+            months = ['January', 'February', 'March', 'April', 'May', 'June', 
+            'July', 'August', 'September', 'October', 'November', 'December'];
+            month = now.getMonth(); // It is zero based, meaning 0 refers to January and 6 refers to May not June.
+
+            year = now.getFullYear();
+            document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
+
+        },
+
+        changedType: function() {
+
+            var fields = document.querySelectorAll(
+                DOMstrings.inputType + ',' + 
+                DOMstrings.inputDescription + ',' +
+                DOMstrings.inputValue
+            );
+
+            nodeListForEach(fields, function(cur) {
+
+                cur.classList.toggle('red-focus');
+
+            });
+
+            document.querySelector(DOMstrings.inputBtn).classList.toggle('red');
 
         },
 
@@ -334,6 +368,8 @@ var controller = (function(budgetCtrl, UICtrl) {
         });
 
         document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
+
+        document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changedType);
 
     };
 
@@ -417,6 +453,7 @@ var controller = (function(budgetCtrl, UICtrl) {
     return {
         init: function() {
             console.log('App has started.');
+            UICtrl.displayDate();
             UICtrl.displayBudget({
                 budget: 0,
                 totalInc: 0,
